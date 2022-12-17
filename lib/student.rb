3ed85ef061aeb322897b6c0a3504882055a2e5d7
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-12-16 21:31:13 -0800

    commit tree ad98b655e35a10852c24e3053356aa9f66a96656

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+  
+  attr_accessor :knowledge
+  
+  #initialize with an instance variable, `@knowledge`, that points to an empty array
+  def initialize
+    @knowledge = []
+  end
+
+  #Define a method, `#learn`, that takes in a string and adds that string to the student's `@knowledge` array /# takes in an argument of a string of knowledge and adds it to the student's knowledge array
+  
+  #Define a method, `#knowledge`, that returns that student's knowledge array
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
 end
